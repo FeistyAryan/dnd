89142cc3de18cfa49719ac08e0740422c5c9be8c
prashant9316 <pgmd.9873@gmail.com>
2024-03-10 21:51:26 +0530

    Made the sub-sub header rendering dynamic based on header data! One Error Still Coming up ('cannot read properties of null (reading 'map'))

--- src/components/body/hero.js
+++ src/components/body/hero.js
@@ -3,7 +3,14 @@ import { Carousel, CarouselItem } from 'react-bootstrap'
 
 export default function hero() {
   return (
-    <Carousel fade>
+    <Carousel fade style={{
+      position: 'absolute',
+      top: '5vh',
+      zIndex: '-10',
+      left: '0px',
+      width: '100%',
+      height: '95vh'
+    }}>
       <Carousel.Item interval={1000}>
         <img style={{width:'100vw',height:'93vh'}} src = 'heroimg1.jpg'/>
       </Carousel.Item>
